59845de8e4d652dc5a6eea860f5a67f342941a6a
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2024-05-15 23:19:35 +0500

    fix: Fixed searching errors in logger module

--- app/logger.py
+++ app/logger.py
@@ -16,17 +16,17 @@ class Logger:
         results = []
 
         for line in FileManager.readLazy("application.log"):
+            line = line.replace("\n", "")
             if not line or len(line) < 23:
                 continue
 
-            contents = line[22:].lower()
+            contents = line[22:].lower().replace("\n", "")
 
             # Python does not support interacting with outer loop
             # so we will use this hacky syntax of throwing an exception
             try:
                 for term in search_terms:
-
-                    if contents.find(term):
+                    if contents.find(term) != -1:
                         results.append(line)
 
                         raise InterruptedError
