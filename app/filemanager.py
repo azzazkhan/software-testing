c09302235e6bae223a2bfd3a8474f52065bc2b97
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2024-05-15 23:11:08 +0500

    feat: Fixed errors + removed debug comments in FileManager

--- app/filemanager.py
+++ app/filemanager.py
@@ -4,44 +4,38 @@ import os
 class FileManager:
     @staticmethod
     def read(path):
-        with open(path, FileManager.buildPath(path), "r") as file:
-            print(f"Reading contents of {path}")
+        with open(FileManager.buildPath(path), "r") as file:
             return file.read()
 
     @staticmethod
     def readLazy(path):
-        with open(path, FileManager.buildPath(path), "r") as file:
-            print(f"Lazily reading contents of {path}")
+        with open(FileManager.buildPath(path), "r") as file:
             return file.readlines()
 
     @staticmethod
     def write(path, contents):
-        with open(path, FileManager.buildPath(path), "w") as file:
-            print(f"Overwriting contents of {path}")
+        with open(FileManager.buildPath(path), "w") as file:
             return file.write(contents)
 
     @staticmethod
     def append(path, contents):
-        print(f"Appending to contents of {path}")
-        with open(path, FileManager.buildPath(path), "a") as file:
+        with open(FileManager.buildPath(path), "a") as file:
             return file.write(contents)
 
     @staticmethod
-    def delete(file_path) -> bool:
-        print(f"Deleting {path}")
-        path = FileManager.buildPath(file_path)
+    def delete(path) -> bool:
+        path = FileManager.buildPath(path)
 
         if os.path.exists(path):
-            print(f"No file exists on {path}")
             os.remove(path)
             return True
 
-        print(f"File deleted successfully {path}")
         return False
 
     @staticmethod
     def buildPath(path: str):
-        return path.join(
-            os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-            path.lstrip("/"),
+        return (
+            os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+            + "/temp/"
+            + path.lstrip("/")
         )
